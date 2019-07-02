085788f76e29912f669b88681ef83876ec3f8c9b
Diego Malafaia <diego.malafaia.ext@dasa.com.br>
2019-07-02 18:08:53 -0300

    Add one more test using get

--- test/contracts/requests/example_request.js
+++ test/contracts/requests/example_request.js
@@ -5,14 +5,14 @@ const expect = require('chai').expect;
 const joiAssert = require('joi-assert');
 
 const {
-    SchemaAirportSearchRadius  
+    SchemaAirportSearchRadius, SchemaSearchTv
 } = require('../schemas/example_schema');
 
 // const URL = '';
 // const PATH = '';
 
 describe('Simple example of conctract test with Joi Assert', () => {     
-    it.only('Validando response com joiAssert', function (done) {
+    it('Example of get request', function (done) {
         request('https://cometari-airportsfinder-v1.p.rapidapi.com/api/airports/')
           .get('by-radius?radius=50&lng=-157.895277&lat=21.265600')
           .set('X-RapidAPI-Host', 'cometari-airportsfinder-v1.p.rapidapi.com')
@@ -21,6 +21,20 @@ describe('Simple example of conctract test with Joi Assert', () => {
           .end(function (err, res) {
             expect(res.status).to.be.eql(200);
             joiAssert(res.body, SchemaAirportSearchRadius);
+            done(err);
+          })
+      });
+
+      it.only('get TV Show content', function (done) {
+        request('https://tvjan-tvmaze-v1.p.rapidapi.com/')
+        .get('shows/2')
+        .set("X-RapidAPI-Host", "tvjan-tvmaze-v1.p.rapidapi.com")
+        .set("X-RapidAPI-Key", "8d3a50857amshcf16e42fbc83c12p1646ebjsn20121bd14040")
+        .expect('Content-Type', /json/)
+          .end(function (err, res) {
+            expect(res.status).to.be.eql(200);
+            joiAssert(res.body, SchemaSearchTv);
+            console.log(res.body)
             done(err);
           })
       });
--- test/contracts/schemas/example_schema.js
+++ test/contracts/schemas/example_schema.js
@@ -17,6 +17,63 @@ const SchemaAirportSearchRadius = Joi.array().items(Joi.object().keys({
     pointsOfSale: Joi.array().required()
 }));
 
+
+const SchemaSearchTv = Joi.object().keys({
+    id: Joi.number().integer().required(),
+    url: Joi.string().required(),
+    name: Joi.string().required(),
+    type: Joi.string().required(),
+    language: Joi.string().required(),
+    genres: Joi.array().required(),
+    status: Joi.string().required(),
+    runtime: Joi.number().integer().required(),
+    premiered: Joi.string().required(),
+    officialSite: Joi.string().required(),
+    schedule: Joi.object().keys({
+        time: Joi.string().required(),
+        days: Joi.array().required()
+    }),
+    rating: Joi.object().keys({
+        average: Joi.number().required()
+    }),
+    weight: Joi.number().required(),
+    network: Joi.object().keys({
+        id: Joi.number().required(),
+        name: Joi.string().required(),
+        country: Joi.object().keys({
+            name: Joi.string().required(),
+            code: Joi.string().required(),
+            timezone: Joi.string().required()    
+        }),
+    }),
+    webChannel: Joi.string().allow(null).required(),
+    externals: Joi.object().keys({
+        tvrage: Joi.number().required(),
+        thetvdb: Joi.number().required(),
+        imdb: Joi.string().required()    
+    }),
+    image: Joi.object().keys({
+        medium: Joi.string().required(),
+        original: Joi.string().required(),
+    }),
+    summary: Joi.string().required(),
+    updated: Joi.number().required(),
+    _links: Joi.object().keys({
+        self: Joi.object().keys({
+            href: Joi.string().required(),
+        }),
+        previousepisode: Joi.object().keys({
+            href: Joi.string().required(),
+        }),
+    }),
+});
+
+
+
+
+
+
 module.exports = {
-    SchemaAirportSearchRadius
+    SchemaAirportSearchRadius,
+    SchemaSearchTv
 }
